efdaddb649e76262f4cf317bcf916e9c03324b18
netanel <netaneal6105@gmail.com>
2022-12-21 13:41:44 +0200

    ok

--- src/layout/header/header.jsx
+++ src/layout/header/header.jsx
@@ -28,11 +28,11 @@ const Header = () => {
                         <img class="d-block w-100" src="#" alt="Third slide" />
                     </div>
                 </div>
-                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <a class="carousel-control-prev-icon" href="#carouselExampleControls" role="button" data-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="sr-only">Previous</span>
                 </a>
-                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <a class="carousel-control-next-icon" href="#carouselExampleControls" role="button" data-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="sr-only">Next</span>
                 </a>
