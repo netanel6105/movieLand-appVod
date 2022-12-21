6088406ba90b97b31c1e8415f867659dfb71e14e
netanel <netaneal6105@gmail.com>
2022-12-21 15:16:32 +0200

    ok

--- src/layout/header/header.jsx
+++ src/layout/header/header.jsx
@@ -2,25 +2,35 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 const Header = () => {
     return (
-        <div className="container-fluid">
 
-            <div className='container'>
-                <nav class="navbar navbar-light bg-light justify-content-between">
-                    <Link class="navbar-brand" to={"#"}>VodApp</Link>
-
-                    <form class="form-inline d-flex">
-                        <input class=" form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button class="btn btn-dark my-2 my-sm-0" type="submit">Search</button>
-                    </form>
-                </nav>
+            <div>
+                
             </div>
+        )
+    }
+    
+    export default Header
+    
+    
+    
 
-        </div>
-
-          
-    )
-}
-
-export default Header
 
 
+
+
+
+    // <div className="container-fluid">
+
+    //     <div className='container'>
+    //         <nav class="navbar navbar-light bg-light justify-content-between">
+    //             <Link class="navbar-brand" to={"#"}>VodApp</Link>
+
+    //             <form class="form-inline d-flex">
+    //                 <input class=" form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+    //                 <button class="btn btn-dark my-2 my-sm-0" type="submit">Search</button>
+    //             </form>
+    //         </nav>
+    //     </div>
+
+
+    // </div>
