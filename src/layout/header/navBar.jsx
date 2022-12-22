34aab95c02e1878301e5ca775ee2211029c818f9
netanel <netaneal6105@gmail.com>
2022-12-22 18:58:39 +0200

    ok

--- src/layout/header/navBar.jsx
+++ src/layout/header/navBar.jsx
@@ -4,10 +4,18 @@ const NavBar = () => {
     return (
         <div>
             <div className="container-fluid">
+
                 <div className='container'>
-        sthths
+                    <nav class="navbar navbar-light bg-light justify-content-between">
+                        <Link class="navbar-brand" to={"#"}>MARICA.TV</Link>
+
+                        <form class="form-inline d-flex">
+                            <input class=" form-control mr-sm-2" type="search" placeholder="Search..." aria-label="Search" />
+                            <button class="btn btn-dark my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                    </nav>
                 </div>
-              
+
 
             </div>
         </div>
