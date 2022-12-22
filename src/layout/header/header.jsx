cf95e6a18eb8d77abcd04734240c7dc1c0f2bb20
netanel <netaneal6105@gmail.com>
2022-12-22 17:06:19 +0200

    OK

--- src/layout/header/header.jsx
+++ src/layout/header/header.jsx
@@ -1,24 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+
+import NavBar from './navBar'
 const Header = () => {
     return (
 
         <div>
-            <div className="container-fluid">
-
-                <div className='container'>
-                    <nav class="navbar navbar-light bg-light justify-content-between">
-                        <Link class="navbar-brand" to={"#"}>VodApp</Link>
-
-                        <form class="form-inline d-flex">
-                            <input class=" form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button class="btn btn-dark my-2 my-sm-0" type="submit">Search</button>
-                        </form>
-                    </nav>
-                </div>
-
-
-            </div>
+            <NavBar/>
+        header
         </div>
     )
 }
--- src/layout/header/navBar.jsx
+++ src/layout/header/navBar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
+    return (
+        <div>
+            <div className="container-fluid">
+
+                <div className='container'>
+                    <nav class="navbar navbar-light bg-light justify-content-between">
+                        <Link class="navbar-brand" to={"#"}>MARICA.TV</Link>
+
+                        <form class="form-inline d-flex">
+                            <input class=" form-control mr-sm-2" type="search" placeholder="Search..." aria-label="Search" />
+                            <button class="btn btn-dark my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                    </nav>
+                </div>
+
+
+            </div>
+        </div>
+    )
 }
 
 export default NavBar 
