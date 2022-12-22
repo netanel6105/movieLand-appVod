5730fb33dd40552984846fd08bf63f86ba3df41b
netanel <netaneal6105@gmail.com>
2022-12-22 17:37:57 +0200

    ok

--- src/layout/header/header.jsx
+++ src/layout/header/header.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
-
 import NavBar from './navBar'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
 const Header = () => {
     return (
 
         <div>
-            <NavBar/>
-        header
+            <NavBar />
+            <Carousel>
+                <div>
+                    <img src="../image/red.jpg" />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src="../image/fast-and-forious.jpg" />
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src="../image/red-notice.webp" />
+                    <p className="legend">Legend 3</p>
+                </div>
+                <div>
+                    <img src="../image/jumanji.jpg" />
+                    <p className="legend">Legend 3</p>
+                </div>
+                <div>
+                    <img src="../image/wekand.webp" />
+                    <p className="legend">Legend 3</p>
+                </div>
+            </Carousel>
         </div>
     )
 }
