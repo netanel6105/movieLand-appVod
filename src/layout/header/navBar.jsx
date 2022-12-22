6dd8fffdb78acdf8c1648614120b439bbf102123
netanel <netaneal6105@gmail.com>
2022-12-22 19:11:00 +0200

    ok

--- src/layout/header/navBar.jsx
+++ src/layout/header/navBar.jsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom'
 const NavBar = () => {
     return (
         <div>
-            <div className="container-fluid">
+            <div className="container-fluid bg-dark">
 
-                <div className='container'>
+                <div className='container '>
                     <nav class="navbar navbar-light bg-light justify-content-between">
-                        <Link class="navbar-brand" to={"#"}>MARICA.TV</Link>
+                        <Link  class="navbar-brand " to={"#"}>MARICA.TV</Link>
 
                         <form class="form-inline d-flex">
                             <input class=" form-control mr-sm-2" type="search" placeholder="Search..." aria-label="Search" />
-                            <button class="btn btn-dark my-2 my-sm-0" type="submit">Search</button>
+                            <button class="btn btn-warning my-2 my-sm-0" type="submit">Search</button>
                         </form>
                     </nav>
                 </div>
