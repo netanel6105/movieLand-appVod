7ff946103d15d1b597f3363b5b578e17ef8ac0bb
netanel <netaneal6105@gmail.com>
2022-12-23 00:05:18 +0200

    ok

--- src/layout/header/header.jsx
+++ src/layout/header/header.jsx
@@ -8,28 +8,35 @@ const Header = () => {
 
         <div className='top '>
             <NavBar />
-            <Carousel>
-                <div>
-                    <img src="../image/red.jpg"/>
-                    <p className="legend">Legend 1</p>
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner ">
+                    <div class="carousel-item active ">
+                        <div class='bg-carousel' style={'../image/red.jpg'}></div>
+                        {/* <img class="d-block w-100" src="..." alt="First slide" /> */}
+                    </div>
+                    <div class="carousel-item">
+                    <div class='bg-carousel'></div>
+                        {/* <img class="d-block w-100" src="..." alt="Second slide" /> */}
+                    </div>
+                    <div class="carousel-item">
+                    <div class='bg-carousel'></div>
+                        {/* <img class="d-block w-100" src="..." alt="Third slide" /> */}
+                    </div>
                 </div>
-                <div>
-                    <img src="../image/fast-and-forious.jpg"  />
-                    <p className="legend">Legend 2</p>
-                </div>
-                <div>
-                    <img src="../image/red-notice.webp" />
-                    <p className="legend">Legend 3</p>
-                </div>
-                <div>
-                    <img src="../image/jumanji.jpg" />
-                    <p className="legend">Legend 3</p>
-                </div>
-                <div>
-                    <img src="../image/wekand.webp" />
-                    <p className="legend">Legend 3</p>
-                </div>
-            </Carousel>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
         </div>
     )
 }
