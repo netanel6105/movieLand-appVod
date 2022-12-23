cda543a578df5a377b4f25ab687708c440e95ff1
netanel <netaneal6105@gmail.com>
2022-12-23 13:46:21 +0200

    ok

--- src/layout/header/header.jsx
+++ src/layout/header/header.jsx
@@ -5,22 +5,14 @@ import NavBar from './navBar'
 const Header = () => {
 
 
-    
+
     return (
 
         <div className='top '>
 
 
-          
-
 
             <NavBar />
-
-
-
-
-
-
 
 
             <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
@@ -30,11 +22,11 @@ const Header = () => {
                         {/* <img src="https://images.pexels.com/photos/33129/popcorn-movie-party-entertainment.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="d-block w-100" alt="..." /> */}
                     </div>
                     <div class="carousel-item">
-                    <div className='bg-carousel2'></div>
+                        <div className='bg-carousel2'></div>
                         {/* <img src="https://images.pexels.com/photos/8273631/pexels-photo-8273631.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="d-block w-100" alt="..." /> */}
                     </div>
                     <div class="carousel-item">
-                    <div className='bg-carousel3'></div>
+                        <div className='bg-carousel3'></div>
                         {/* <img src="https://images.pexels.com/photos/7991142/pexels-photo-7991142.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="d-block w-100" alt="..." /> */}
                     </div>
                 </div>
