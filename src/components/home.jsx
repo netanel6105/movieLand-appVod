7f84190801cb73ccafd20a59cda98086cb154ff3
netanel <netaneal6105@gmail.com>
2022-12-24 21:55:21 +0200

    ok

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,20 +1,37 @@
+import axios from 'axios'
 import React from 'react'
 import { useEffect } from 'react'
+import { useState } from 'react'
+import { useParams } from "react-router-dom";
 
 const Home = () => {
 
-
+    const [data, setData] = useState({});
+    const [loading, setLoading] = useState(true)
+    const param = useParams()
 
     const doApi = async() =>{
-
+        try{
+            
+            setLoading(true)
+            let url = `http://www.omdbapi.com/?s=${param.Search || "bank"}&apikey=c253e9ac`
+            const {data} = await axios.get(url);
+            console.log(data.Search);
+            setData(data.Search);
+            setLoading(false);
+        }
+        catch(error){
+            console.log(error.response);
+        }
+        
     }
 
 
 
 
-    useEffect({
-
-    },[])
+    useEffect(()=>{
+        doApi();
+    },[param])
 
 
 
@@ -24,8 +41,14 @@ const Home = () => {
 
 
     return (
-        <div>
-            home
+        
+        <div className='container-fluid'>
+            <div className="container">
+                <div className="row">
+               
+                </div>
+            </div>
+            
         </div>
     )
 }
