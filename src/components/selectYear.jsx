ed707854e0eafd7befcfef6efeaca0b43f99f2e6
netanel <netaneal6105@gmail.com>
2022-12-24 19:26:00 +0200

    ok

--- src/components/selectYear.jsx
+++ src/components/selectYear.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 
 const SelectYear = () => {
-  return (
-    <div className='topHome container-fluid '>
-            <div className="container p-2">
-                <select className='form-select w-25'>
-                    <option value="#">select-year</option>
-                </select>
-            </div>
 
+
+    return (
+        <div className='topHome container-fluid '>
+            <div className="container-md">
+                <div className="row col-lg-2 col-md-4 col-4">
+                   <h2 className='topYear mb-2 mt-2 '>Top Yaer</h2>
+                   
+                    <select className='form-select mb-2'>
+                        <option value="#">select-year...  </option>
+                    </select>
+                </div>
+            </div>
+           
         </div>
-  )
+    )
 }
 
 export default SelectYear
--- src/layout/header/navBar.jsx
+++ src/layout/header/navBar.jsx
@@ -12,12 +12,12 @@ const NavBar = () => {
             <div className="navTop container-fluid  p-2">
 
                 <div className='container-md'>
-                    <div className="row">
+                    <div className="row ">
                         <nav class="navbar navbar-light  justify-content-between">
-                            <Link class="navbar-brand" to={"/"}> <h3 className='nameApp  my-2 p-1 ' >VECTOR.TV</h3> </Link>
+                            <Link class="navbar-brand" to={"/"}> <h3 className='nameApp  my-2 p-1' >VECTOR.TV</h3> </Link>
 
-                            <form class="form-inline d-flex">
-                                <input onKeyDown={(e) => { if (e.key === "Enter") { nav("/" + inputRef.current.value) } }} ref={inputRef} class="inp-1 form-control  mr-sm-2" type="search" placeholder="Search..." aria-label="Search" />
+                            <form class="form-inline d-flex ">
+                                <input onKeyDown={(e) => { if (e.key === "Enter") { nav("/" + inputRef.current.value) } }} ref={inputRef} class="inp-1 form-control   mr-sm-2" type="search" placeholder="Search..." aria-label="Search" />
                                 <button onClick={() => nav(inputRef.current.value)} class="bot-1 btn btn-warning   my-1 mx-0 my-sm-0" type="submit">Search</button>
                             </form>
                         </nav>
