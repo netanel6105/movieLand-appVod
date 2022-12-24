b80e06e7ad560c50f5259983d2cdaa4f190b4c54
netanel <netaneal6105@gmail.com>
2022-12-24 23:01:44 +0200

    ok

--- src/layout/header/navBar.jsx
+++ src/layout/header/navBar.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
     const inputRef = useRef();
-    const nav = useNavigate()
+    const nav = useNavigate();
 
     return (
         <div>
@@ -18,7 +18,8 @@ const NavBar = () => {
                             <Link class="navbar-brand" to={"/"}> <h3 className='nameApp  my-2 p-1' >VECTOR.TV</h3> </Link>
 
                             <form class="form-inline d-flex ">
-                                <input onKeyDown={(e) =>  { if (e.key === "Enter") { nav("/" + inputRef.current.value) }  }} ref={inputRef} class="inp-1 form-control appearance-none   mr-sm-2" type="search" placeholder="Search..." aria-label="Search" />
+                                <input onKeyDown={(e) =>  { if (e.key === "Enter") {e.preventDefault()} { nav("/" + inputRef.current.value) }  }} ref={inputRef} class="inp-1 form-control appearance-none   mr-sm-2" type="search" placeholder="Search..." aria-label="Search" />
+                               
                                 <button onClick={() => {nav(  "/" + inputRef.current.value)}} class="bot-1 btn btn-warning   my-1 mx-0 my-sm-0" type="submit">Search</button>
                             </form>
                         </nav>
