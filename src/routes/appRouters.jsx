440542773601177ec7640f5629f9cc097231536a
netanel <netaneal6105@gmail.com>
2022-12-28 18:34:53 +0200

    ko

---
+++ src/routes/appRouters.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+const AppRouters = () => {
+  return (
+    <div>AppRouters</div>
+  )
+}
+
+export default AppRouters
