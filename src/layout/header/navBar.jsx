16bdc684235333d587e916cf3bd605d2e6f9cbc9
netanel <netaneal6105@gmail.com>
2023-01-05 10:55:29 +0200

    ok

--- src/layout/header/navBar.jsx
+++ src/layout/header/navBar.jsx
@@ -1,6 +1,6 @@
-// import e from 'react';
+
+
 import React, { useRef } from 'react'
-// import { useState } from 'react';
 import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom';
 
