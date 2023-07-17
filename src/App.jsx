2576cd3124e7c982a3f88619ae30445f879b59a8
netanel <netaneal6105@gmail.com>
2023-07-17 20:14:57 +0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css';
+import TestGraph from './comps/testGraph';
 
 function App() {
   return (
    
-    <div>
-      
+    <div className='App'>
+      <TestGraph/>
     </div>
   );
 }
