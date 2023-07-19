9e252bc85d9c23a45ec445e019457f6368cdb032
netanel <netaneal6105@gmail.com>
2023-07-19 21:44:21 +0300

    ok

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TestGraph from './comps/testGraph';
+
+function App() {
+  return (
+    <div className="App">
+     <TestGraph />
+    </div>
+  );
+}
+
+export default App;
--- src/comps/testGraph.js
+++ src/comps/testGraph.js
@@ -1,53 +1,59 @@
-import React from 'react'
-import CanvasJSReact from '../grafh_lib/canvasjs.react'
+import React, { useEffect } from 'react'
+import CanvasJSReact from '../graph_lib/canvasjs.react';
+import axios from "axios"
 const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
-const TestGraph = () => {
-	const options = {
-        animationEnabled: true,
-        exportEnabled: true,
-        theme: "light2", //"light1", "dark1", "dark2"
-        title:{
-            text: "Simple Column Chart with Index Labels"
-        },
-        axisY: {
-            includeZero: true
-        },
-        data: [{
-            type: "column", //change type to bar, line, area, pie, etc
-            //indexLabel: "{y}", //Shows y value on all Data Points
-            indexLabelFontColor: "#5A5757",
-            indexLabelPlacement: "outside",
-            dataPoints: [
-                { x: 10, y: 71 },
-                { x: 20, y: 55 },
-                { x: 30, y: 50 },
-                { x: 40, y: 65 },
-                { x: 50, y: 71 },
-                { x: 60, y: 68 },
-                { x: 70, y: 38 },
-                { x: 80, y: 92, indexLabel: "Highest" },
-                { x: 90, y: 54 },
-                { x: 100, y: 60 },
-                { x: 110, y: 21 },
-                { x: 120, y: 49 },
-                { x: 130, y: 36 }
-            ]
-        }]
-    }
+
+export default function TestGraph() {
+
+  useEffect(() => {
+    
+    doApi();
+  },[])
+
+  const doApi = async() => {
+    let url = "https://coronavirus-19-api.herokuapp.com/countries";
+    const resp = await axios.get(url);
+    console.log(resp.data)
+  }
+
+  const ar = [
+    { label:"Jerusalem" , y:900},
+    { label:"Tel aviv" , y:800},
+    { label:"Ramat gan" , y:700},
+    {label:"Netanya" , y:750}
+  ]
+  
+  const options = {
+    animationEnabled: true,
+    exportEnabled: true,
+    theme: "light2", //"light1", "dark1", "dark2"
+    title:{
+      text: "Graph of pop in cities"
+    },
+    axisY: {
+      includeZero: false,
+      title:"Pop * 1000"
+    },
+    axisX: {
+      title:"Cities"
+    },
+    data: [{
+      type: "column", //change type to bar, line, area, pie, etc
+      // indexLabel: "{y}", //Shows y value on all Data Points
+      indexLabelFontColor: "#5A5757",
+      indexLabelPlacement: "outside",
+      dataPoints: ar
+    }]
+  }
 
   return (
     <div className='container'>
-        <h2>Graph of something: </h2>
+      <h2>Graph of something</h2>
 
-        <CanvasJSChart options = {options} 
+      <CanvasJSChart options = {options} 
 				/* onRef={ref => this.chart = ref} */
-				/* containerProps={{ width: '100%', height: '300px' }} */
 			/>
-        
-        
     </div>
   )
 }
-
-export default TestGraph
